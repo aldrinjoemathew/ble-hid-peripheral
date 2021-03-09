14003a6cba39b4208623b24d5a38046e06f25c64
Aldrin Joe Mathew <aldrin@qburst.com>
2021-03-09 17:21:53 +0530

    Updated to latest

--- lib/src/main/java/jp/kshoji/blehid/HidPeripheral.java
+++ lib/src/main/java/jp/kshoji/blehid/HidPeripheral.java
@@ -23,8 +23,8 @@ import android.content.IntentFilter;
 import android.os.Build.VERSION_CODES;
 import android.os.Handler;
 import android.os.ParcelUuid;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import java.nio.charset.StandardCharsets;
